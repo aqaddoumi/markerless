4ad7b9e896d07c2bd3a5be475e1fec83f3bb84cf
aqaddoumi <qaddoumi.adel@gmail.com>
2020-04-15 23:52:09 +0300

    markerless

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+var path = require('path');
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World</h1>');
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(3000, () => {
+app.listen(5000, () => {
   console.log('Server is up on 3000');
 });
