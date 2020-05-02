9160ed802760b76a78b46979979678adcb4f3c47
aqaddoumi <qaddoumi.adel@gmail.com>
2020-05-02 22:06:27 +0300

    interface

--- interface/markerless.js
+++ interface/markerless.js
@@ -1,7 +1,6 @@
 const xrScene = `
 <a-scene xrweb xrextras-almost-there xrextras-loading xrextras-runtime-error>
-  <a-camera id="camera-entity" position="0 0 0" raycaster="objects: .cantap" cursor="fuse: false; rayOrigin: mouse;"></a-camera>
-  <a-box id="ground" class="cantap" scale="1000 2 1000" position="0 -1 0" material="shader: shadow; transparent: true; opacity: 0.4" shadow></a-box>
+  <a-box position="0 0 -5"></a-box>
 </a-scene>
 `;
 
