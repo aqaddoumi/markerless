63fedb316b261af2f222deb9c09fa4e47a6aef82
aqaddoumi <qaddoumi.adel@gmail.com>
2020-05-02 22:10:24 +0300

    interface

--- interface/markerless.js
+++ interface/markerless.js
@@ -1,5 +1,5 @@
 const xrScene = `
-<a-scene xrweb xrextras-almost-there xrextras-loading xrextras-runtime-error>
+<a-scene xrweb xrextras-almost-there xrextras-loading xrextras-runtime-error xrextras-log-to-screen>
   <a-camera position="0 0 0"></a-camera>
   <a-box position="0 0 -5"></a-box>
 </a-scene>
