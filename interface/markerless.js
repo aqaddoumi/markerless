e6f10c33f2ebe005c299df767f2fe08e5894acfe
aqaddoumi <qaddoumi.adel@gmail.com>
2020-05-02 22:14:20 +0300

    interface

--- interface/markerless.js
+++ interface/markerless.js
@@ -1,5 +1,9 @@
+const xrScreen = `
+<div style="background-color: red; height: 100%; width: 100%;"></div>
+`;
+
 const xrScene = `
-<a-scene xrweb xrextras-almost-there xrextras-loading xrextras-runtime-error xrextras-log-to-screen>
+<a-scene xrweb xrextras-almost-there xrextras-loading xrextras-runtime-error>
   <a-camera position="0 0 0"></a-camera>
   <a-box position="0 0 -5"></a-box>
 </a-scene>
@@ -8,5 +12,8 @@ const xrScene = `
 window.XRExtras.AFrame.loadAFrameForXr({
   version: 'latest',
 }).then(() => {
+  document.body.insertAdjacentHTML('beforeend', xrScreen);
   document.body.insertAdjacentHTML('beforeend', xrScene);
 });
+
+// xrextras-log-to-screen
