6248aeaf954992bd0766fe300de833e4e0a6788f
aqaddoumi <qaddoumi.adel@gmail.com>
2020-05-02 22:21:58 +0300

    interface

--- interface/markerless.js
+++ interface/markerless.js
@@ -1,5 +1,5 @@
 /*const xrScreen = `
-<div id="interface-container" style="background-color: red; height: 100%; width: 100%;"></div>
+
 `;*/
 
 const xrScene = `
@@ -17,3 +17,42 @@ window.XRExtras.AFrame.loadAFrameForXr({
 });
 
 // xrextras-log-to-screen
+
+/*<!DOCTYPE html>
+<html>
+  <head>
+    <title>TalkAR</title>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <meta name="apple-mobile-web-app-capable" content="yes">
+
+  </head>
+  <body>
+    <div class="container-fluid" id="interface-container">
+        <div class="row justify-content-center align-items-center" style="height: 20%;">
+          <div class="col-10">
+            <h3 class="text-center gotham-font">AIM CAMERA TOWARD ANY FLAT SURFACE</h1>
+          </div>
+        </div>
+        <div class="row justify-content-center" style="height: 15%;"></div>
+        <div class="row justify-content-center" style="height: 30%;">
+          <div class="col-10 justify-content-center" style="position: relative;">
+            <img id="tap-circle" src="assets/tap-circle.png"/>
+            <img id="tap-hand" src="assets/tap-hand.png"/>
+          </div>
+        </div>
+        <div class="row justify-content-center" style="height: 15%;"></div>
+        <div class="row justify-content-center align-items-center" style="height: 20%;">
+          <div class="col-10">
+            <h3 class="text-center gotham-font">TAP TO VIEW AR HOLOGRAM</h1>
+          </div>
+        </div>
+      </div>
+
+  </body>
+</html>
+
+*/
