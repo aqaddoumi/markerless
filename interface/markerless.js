d96f7a9b9f533f18b32f8efb36304c469d3ff55f
aqaddoumi <qaddoumi.adel@gmail.com>
2020-05-04 00:09:22 +0300

    interface

--- interface/markerless.js
+++ interface/markerless.js
@@ -1,7 +1,3 @@
-/*const xrScreen = `
-
-`;*/
-
 const xrScene = `
 <a-scene xrweb xrextras-almost-there xrextras-loading xrextras-runtime-error>
   <a-camera position="0 0 0"></a-camera>
@@ -13,46 +9,11 @@ window.XRExtras.AFrame.loadAFrameForXr({
   version: 'latest',
 }).then(() => {
   document.body.insertAdjacentHTML('beforeend', xrScene);
-  //document.body.insertAdjacentHTML('afterbegin', xrScreen);
 });
 
-// xrextras-log-to-screen
-
-/*<!DOCTYPE html>
-<html>
-  <head>
-    <title>TalkAR</title>
-    <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, shrink-to-fit=no"
-    />
-    <meta name="apple-mobile-web-app-capable" content="yes">
-
-  </head>
-  <body>
-    <div class="container-fluid" id="interface-container">
-        <div class="row justify-content-center align-items-center" style="height: 20%;">
-          <div class="col-10">
-            <h3 class="text-center gotham-font">AIM CAMERA TOWARD ANY FLAT SURFACE</h1>
-          </div>
-        </div>
-        <div class="row justify-content-center" style="height: 15%;"></div>
-        <div class="row justify-content-center" style="height: 30%;">
-          <div class="col-10 justify-content-center" style="position: relative;">
-            <img id="tap-circle" src="assets/tap-circle.png"/>
-            <img id="tap-hand" src="assets/tap-hand.png"/>
-          </div>
-        </div>
-        <div class="row justify-content-center" style="height: 15%;"></div>
-        <div class="row justify-content-center align-items-center" style="height: 20%;">
-          <div class="col-10">
-            <h3 class="text-center gotham-font">TAP TO VIEW AR HOLOGRAM</h1>
-          </div>
-        </div>
-      </div>
-
-  </body>
-</html>
-
-*/
+/*   <script
+async
+src="https://apps.8thwall.com/xrweb?appKey=6GSBAjDK5sqxliljtu4BiY8P6OMN1rH5t9ZjxeOjJt0JlQeO8aIyK4ccQMER1cGbH5cmSE"
+></script>
+<script src="./../xrextras/dist/xrextras.js"></script>
+<script src="./markerless.js"></script>*/
