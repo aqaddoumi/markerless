b3c7ac2266c87d4ec14eceb5ad5bd05166146a63
aqaddoumi <qaddoumi.adel@gmail.com>
2020-05-04 00:13:02 +0300

    interface

--- interface/markerless.js
+++ interface/markerless.js
@@ -10,10 +10,3 @@ window.XRExtras.AFrame.loadAFrameForXr({
 }).then(() => {
   document.body.insertAdjacentHTML('beforeend', xrScene);
 });
-
-/*   <script
-async
-src="https://apps.8thwall.com/xrweb?appKey=6GSBAjDK5sqxliljtu4BiY8P6OMN1rH5t9ZjxeOjJt0JlQeO8aIyK4ccQMER1cGbH5cmSE"
-></script>
-<script src="./../xrextras/dist/xrextras.js"></script>
-<script src="./markerless.js"></script>*/
