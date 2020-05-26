101dbf651ecd712b4eb8854b49480ba6829a3e05
aqaddoumi <qaddoumi.adel@gmail.com>
2020-05-26 17:27:44 +0300

    qrcode

--- xrextras/src/almosttheremodule/almost-there-module.js
+++ xrextras/src/almosttheremodule/almost-there-module.js
@@ -1,78 +1,81 @@
-require('!style-loader!css-loader!../fonts/fonts.css')
-require('!style-loader!css-loader!./almost-there-module.css')
+require('!style-loader!css-loader!../fonts/fonts.css');
+require('!style-loader!css-loader!./almost-there-module.css');
 
-const html = require('./almost-there-module.html')
+const html = require('./almost-there-module.html');
 
-let almostThereModule = null
+let almostThereModule = null;
 
 const AlmostThereFactory = () => {
   if (!almostThereModule) {
-    almostThereModule = create()
+    almostThereModule = create();
   }
 
-  return almostThereModule
-}
+  return almostThereModule;
+};
 
 function create() {
-  let rootNode = null
-  let customRedirectUrl
+  let rootNode = null;
+  let customRedirectUrl;
   const showId = (id) => {
-    document.getElementById(id).classList.remove('hidden')
-  }
+    document.getElementById(id).classList.remove('hidden');
+  };
 
   const hideAlmostThere = () => {
     if (!rootNode) {
-      return
+      return;
     }
-    rootNode.parentNode.removeChild(rootNode)
-    rootNode = null
-  }
+    rootNode.parentNode.removeChild(rootNode);
+    rootNode = null;
+  };
 
   const showAlmostThere = () => {
-    const e = document.createElement('template')
-    e.innerHTML = html.trim()
-    rootNode = e.content.firstChild
-    document.getElementsByTagName('body')[0].appendChild(rootNode)
+    const e = document.createElement('template');
+    e.innerHTML = html.trim();
+    rootNode = e.content.firstChild;
+    document.getElementsByTagName('body')[0].appendChild(rootNode);
 
-    const redirectUrl = customRedirectUrl || window.location.href
-    const redirectLinks = rootNode.querySelectorAll('.desktop-home-link')
+    const redirectUrl = customRedirectUrl || window.location.href;
+    const redirectLinks = rootNode.querySelectorAll('.desktop-home-link');
     for (let i = 0; i < redirectLinks.length; i++) {
-      redirectLinks[i].textContent = redirectUrl
+      redirectLinks[i].textContent = redirectUrl;
     }
 
-    const reasons = XR8.XrDevice.incompatibleReasons()
-    const details = XR8.XrDevice.incompatibleReasonDetails()
-    const device = XR8.XrDevice.deviceEstimate()
+    const reasons = XR8.XrDevice.incompatibleReasons();
+    const details = XR8.XrDevice.incompatibleReasonDetails();
+    const device = XR8.XrDevice.deviceEstimate();
 
-    const ogTag = document.querySelector('meta[name="og:image"]')
-    const headerImgSrc = ogTag && ogTag.content
+    const ogTag = document.querySelector('meta[name="og:image"]');
+    const headerImgSrc = ogTag && ogTag.content;
     Array.from(document.querySelectorAll('.app-header-img')).forEach((img) => {
       if (headerImgSrc) {
-        img.src = headerImgSrc
+        img.src = headerImgSrc;
       } else {
-        img.classList.add('foreground-image')
-        img.src = 'https://cdn.8thwall.com/web/img/almostthere/v2/safari-fallback.png'
+        img.classList.add('foreground-image');
+        img.src =
+          'https://cdn.8thwall.com/web/img/almostthere/v2/safari-fallback.png';
       }
-    })
+    });
 
-    const cBtn = document.getElementById('error_copy_link_btn')
+    const cBtn = document.getElementById('error_copy_link_btn');
     cBtn.addEventListener('click', () => {
-      const dummy = document.createElement('input')
-      document.body.appendChild(dummy)
-      dummy.value = redirectUrl
-      dummy.select()
-      document.execCommand('copy')
-      document.body.removeChild(dummy)
+      const dummy = document.createElement('input');
+      document.body.appendChild(dummy);
+      dummy.value = redirectUrl;
+      dummy.select();
+      document.execCommand('copy');
+      document.body.removeChild(dummy);
 
-      cBtn.innerHTML = 'Copied!'
-      cBtn.classList.add('error-copy-link-copied')
-    })
+      cBtn.innerHTML = 'Copied!';
+      cBtn.classList.add('error-copy-link-copied');
+    });
 
-    if (reasons.includes(XR8.XrDevice.IncompatibilityReasons.UNSUPPORTED_BROWSER)) {
+    if (
+      reasons.includes(XR8.XrDevice.IncompatibilityReasons.UNSUPPORTED_BROWSER)
+    ) {
       if (device.os === 'iOS') {
         if (details.inAppBrowserType == 'Safari') {
-          showId('error_msg_open_in_safari')
-          showId('apple_open_safari_hint')
+          showId('error_msg_open_in_safari');
+          showId('apple_open_safari_hint');
         } else {
           switch (details.inAppBrowser) {
             case 'Instagram':
@@ -82,84 +85,97 @@ function create() {
             case 'QQ':
             case 'Sino Weibo':
             case 'Snapchat':
-              showId('error_msg_open_in_safari')
-              showId('error_text_header_top')
-              showId('top_corner_open_safari')
+              showId('error_msg_open_in_safari');
+              showId('error_text_header_top');
+              showId('top_corner_open_safari');
               if (details.inAppBrowser === 'Instagram') {
-                document.body.classList.add('bottombarbump')
+                document.body.classList.add('bottombarbump');
               }
-              break
+              break;
             case 'Facebook Messenger':
             case 'Kakao Talk':
             case 'Naver':
-              showId('error_msg_open_in_safari')
-              showId('error_text_header_bottom')
-              showId('bottom_corner_open_safari')
-              break
+              showId('error_msg_open_in_safari');
+              showId('error_text_header_bottom');
+              showId('bottom_corner_open_safari');
+              break;
             case 'Line':
             case 'Mozilla Firefox Focus':
-              showId('error_msg_open_in_safari')
-              showId('error_text_header_top')
-              showId('top_close_open_safari')
-              break
+              showId('error_msg_open_in_safari');
+              showId('error_text_header_top');
+              showId('top_close_open_safari');
+              break;
             default:
-              showId('error_unknown_webview')
-              break
+              showId('error_unknown_webview');
+              break;
           }
         }
-        return
+        return;
       }
     }
 
-    if (reasons.includes(XR8.XrDevice.IncompatibilityReasons.MISSING_WEB_ASSEMBLY)) {
+    if (
+      reasons.includes(XR8.XrDevice.IncompatibilityReasons.MISSING_WEB_ASSEMBLY)
+    ) {
       if (device.os == 'iOS') {
-        showId('error_msg_web_assembly_ios')
-        return
+        showId('error_msg_web_assembly_ios');
+        return;
       }
       if (device.os == 'Android') {
-        showId('error_msg_web_assembly_android')
-        return
+        showId('error_msg_web_assembly_android');
+        return;
       }
     }
 
     if (device.os == 'iOS') {
-      showId('error_unknown_webview')
-      showId('error_text_header_unknown')
-      return
+      showId('error_unknown_webview');
+      showId('error_text_header_unknown');
+      return;
     }
 
     if (device.os == 'Android') {
-      showId('error_msg_android_almost_there')
-      return
+      showId('error_msg_android_almost_there');
+      return;
     }
 
     // Desktop: help our user with a qr code
-    showId('error_msg_device')
-    const scriptElem = document.createElement('script')
-    scriptElem.type = 'text/javascript'
-    scriptElem.src = 'https://cdn.8thwall.com/web/share/qrcode8-1.1.0.js'
+    showId('error_msg_device');
+    const scriptElem = document.createElement('script');
+    scriptElem.type = 'text/javascript';
+    //scriptElem.src = 'https://cdn.8thwall.com/web/share/qrcode8-1.1.0.js'
+    scriptElem.src = './qrcode.min.js';
     scriptElem.onload = () => {
-      document.getElementById('qrcode').innerHTML = qrcode8.generateQR8Svg(redirectUrl, 250, 80)
-    }
-    document.getElementById('almostthereContainer').appendChild(scriptElem)
-  }
+      //document.getElementById('qrcode').innerHTML = qrcode8.generateQR8Svg(redirectUrl, 250, 80)
+      var config = {};
+      config.msg = redirectUrl;
+      config.dim = 256;
+      config.pad = 0;
+      config.mtx = -1;
+      config.ecl = 'H';
+      config.ecb = 1;
+      config.pal = ['#187BF2', '#fff'];
+      config.vrb = 0;
+      document.getElementById('qrcode').innerHTML = new QRCode(config);
+    };
+    document.getElementById('almostthereContainer').appendChild(scriptElem);
+  };
 
   const checkCompatibility = () => {
     if (rootNode) {
-      return false
+      return false;
     }
 
     if (XR8.XrDevice.isDeviceBrowserCompatible()) {
       // Everything is ok.
-      return true
+      return true;
     }
 
-    showAlmostThere()
+    showAlmostThere();
 
-    XR8.pause()
-    XR8.stop()
-    return false
-  }
+    XR8.pause();
+    XR8.stop();
+    return false;
+  };
 
   const pipelineModule = () => ({
     name: 'almostthere',
@@ -167,28 +183,28 @@ function create() {
       if (!checkCompatibility()) {
         // Throwing an error here allows other pipeline modules to react in their onException
         // methods.
-        throw Error('Device or browser incompatible with XR.')
+        throw Error('Device or browser incompatible with XR.');
       }
     },
     onException: () => {
-      checkCompatibility()
+      checkCompatibility();
     },
-  })
+  });
 
-  const configure = ({url}) => {
+  const configure = ({ url }) => {
     if (url !== undefined) {
-      customRedirectUrl = url
+      customRedirectUrl = url;
     }
-  }
+  };
 
   return {
     pipelineModule,
     checkCompatibility,
     configure,
     hideAlmostThere,
-  }
+  };
 }
 
 module.exports = {
   AlmostThereFactory,
-}
+};
